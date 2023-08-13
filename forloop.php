4571d1fbef6f88b5c3375a647856e57be54bacf7
wahyulut <lutvi.190170110@gmail.com>
2023-08-13 15:19:09 +0700

    Perulangan menggunakan for

---
+++ forloop.php
@@ -0,0 +1,12 @@
+<?php 
+
+    for($counter = 1 ; $counter <= 10 ; $counter++) { 
+        
+        echo "Hello world, ini adalah perulangan ke-$counter" . PHP_EOL;
+
+    }
+    for ($i=10; $i >= 1 ; $i--) {
+
+        echo "ini perulangan menurun dari $i" . PHP_EOL;
+    }
+?>
